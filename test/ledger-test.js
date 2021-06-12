c44692945267f3a04a372fb62effcb2a46b259f1
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 14:55:29 -0400

    Add test for bookings property

--- test/ledger-test.js
+++ test/ledger-test.js
@@ -59,5 +59,72 @@ describe('Ledger Class', () => {
     ]);
   });
 
-
+  it('Should hold an array of all Booking instances', () => {
+    expect(ledger.bookings[0]).to.be.an.instanceof(Booking);
+    expect(ledger.bookings).to.deep.equal([
+      {
+        'id': '5fwrgu4i7k55hl6sz',
+        'userID': 2,
+        'date': '2020/01/22',
+        'roomNumber': 1,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6t5',
+        'userID': 4,
+        'date': '2020/01/24',
+        'roomNumber': 4,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6t6',
+        'userID': 3,
+        'date': '2020/01/10',
+        'roomNumber': 2,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6t7',
+        'userID': 1,
+        'date': '2020/01/16',
+        'roomNumber': 5,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6t8',
+        'userID': 1,
+        'date': '2020/01/05',
+        'roomNumber': 3,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6t9',
+        'userID': 5,
+        'date': '2020/01/14',
+        'roomNumber': 3,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6ta',
+        'userID': 2,
+        'date': '2020/01/11',
+        'roomNumber': 5,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6tb',
+        'userID': 1,
+        'date': '2020/01/11',
+        'roomNumber': 4,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6tc',
+        'userID': 3,
+        'date':'2020/01/30',
+        'roomNumber': 4,
+        'roomServiceCharges': []
+      }
+    ]);
+  });
 });
