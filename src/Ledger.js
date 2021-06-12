1356358f1d13bcab53b33e12ca25066646acefaa
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 15:28:50 -0400

    Add property and method to handle list of rooms

--- src/Ledger.js
+++ src/Ledger.js
@@ -1,5 +1,18 @@
+import Room from './Room';
+
 class Ledger {
-  constructor() {}
+  constructor(roomData, bookingData) {
+    this.rooms = [];
+
+    this.consolidateRooms(roomData);
+  }
+
+  consolidateRooms(roomData) {
+    roomData.forEach(room => {
+      const newRoom = new Room(room);
+      this.rooms.push(newRoom);
+    });
+  }
 }
 
 export default Ledger;
