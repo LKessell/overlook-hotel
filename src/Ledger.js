3000ec554a6c71d2d4dc31740ffd6f2a638ede58
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 15:32:00 -0400

    Add property and method to handle list of bookings

--- src/Ledger.js
+++ src/Ledger.js
@@ -1,16 +1,26 @@
 import Room from './Room';
+import Booking from './Booking';
 
 class Ledger {
   constructor(roomData, bookingData) {
     this.rooms = [];
+    this.bookings = [];
 
     this.consolidateRooms(roomData);
+    this.consolidateBookings(bookingData);
   }
 
   consolidateRooms(roomData) {
     roomData.forEach(room => {
       const newRoom = new Room(room);
       this.rooms.push(newRoom);
+    });
+  }
+
+  consolidateBookings(bookingData) {
+    bookingData.forEach(booking => {
+      const newBooking = new Booking(booking);
+      this.bookings.push(newBooking);
     });
   }
 }
