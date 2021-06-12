ab5bb85c0a7caeda35b8429e4d11994c2e6cec6a
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 16:37:15 -0400

    Add method to get customer-specific bookings

--- src/Customer.js
+++ src/Customer.js
@@ -1,7 +1,15 @@
 class Customer {
-  constructor(customerData) {
+  constructor(customerData, bookingData) {
     this.id = customerData.id;
     this.name = customerData.name;
+    this.bookings = [];
+
+    this.getBookings(bookingData);
+  }
+
+  getBookings(bookingData) {
+    const matchedBookings = bookingData.filter(booking => booking.userID === this.id);
+    this.bookings = matchedBookings;
   }
 }
 
