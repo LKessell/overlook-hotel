d2b38cb61e4dd559090cfee35d0ffef78d2f0d9f
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 12:22:35 -0400

    Add tests for bedSize, numBeds, costPerNight properties

--- test/room-test.js
+++ test/room-test.js
@@ -23,11 +23,23 @@ describe.only('Room Class', () => {
     expect(room.number).to.equal(1);
   });
 
-  it('Should have a roomType', () => {
+  it('Should have a room type', () => {
     expect(room.roomType).to.equal('residential suite');
   });
 
   it('Should have a bidet property', () => {
     expect(room.bidet).to.equal(true);
+  });
+
+  it('Should have a bed size', () => {
+    expect(room.bedSize).to.equal('queen');
+  });
+
+  it('Should have a number of beds', () => {
+    expect(room.numBeds).to.equal(1);
+  });
+
+  it('Should have a cost per night', () => {
+    expect(room.costPerNight).to.equal(358.4);
   });
 });
