0a6ae3ca4b43c65fbd802482f7a0e5f8c9128e7d
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 15:03:13 -0400

    Add test to get available rooms on a given date

--- test/ledger-test.js
+++ test/ledger-test.js
@@ -127,4 +127,33 @@ describe('Ledger Class', () => {
       }
     ]);
   });
+
+  it('Should return all rooms without a booking on a given date', () => {
+    expect(ledger.getAvailableRooms('2020-01-11')).to.deep.equal([
+      {
+        'number': 1,
+        'roomType': 'residential suite',
+        'bidet': true,
+        'bedSize': 'queen',
+        'numBeds': 1,
+        'costPerNight': 358.4
+      },
+      {
+        'number': 2,
+        'roomType': 'suite',
+        'bidet': false ,
+        'bedSize': 'full',
+        'numBeds': 2,
+        'costPerNight': 477.38
+      },
+      {
+        'number': 3,
+        'roomType': 'single room',
+        'bidet': false,
+        'bedSize': 'king',
+        'numBeds': 1,
+        'costPerNight': 491.14
+      }
+    ]);
+  });
 });
