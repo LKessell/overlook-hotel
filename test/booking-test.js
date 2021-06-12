b276981c42acf702d5e84d4e2dccd9cbb672f62b
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 12:45:55 -0400

    Add tests for instantiation

--- test/booking-test.js
+++ test/booking-test.js
@@ -5,5 +5,17 @@ import bookingsTestData from './test-data/bookings-data';
 import Booking from '../src/Booking';
 
 describe('Booking Class', () => {
+  let booking;
 
+  beforeEach(() => {
+    booking = new Booking(bookingsTestData[0]);
+  });
+
+  it('Should be a function', () => {
+    expect(Booking).to.be.a('function');
+  });
+
+  it('Should be an instance of the Booking class', () => {
+    expect(booking).to.be.an.instanceof(Booking);
+  });
 });
