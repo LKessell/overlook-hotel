2359ff13e4b431b2359246489d5b77c795cb9e09
Lauren Kessell <lkessell1@gmail.com>
2021-06-12 10:52:29 -0400

    Fix syntax error

--- test/customer-test.js
+++ test/customer-test.js
@@ -35,56 +35,60 @@ describe('Customer Class', () => {
   });
 
   it('Should store a list of bookings', () => {
-    expect(customer.bookings).to.deep.equal({
-      'id': '5fwrgu4i7k55hl6t7',
-      'userID': 1,
-      'date': '2020/01/16',
-      'roomNumber': 5,
-      'roomServiceCharges': []
-    },
-    {
-      'id': '5fwrgu4i7k55hl6t8',
-      'userID': 1,
-      'date': '2020/01/05',
-      'roomNumber': 3,
-      'roomServiceCharges': []
-    },
-    {
-      'id': '5fwrgu4i7k55hl6tb',
-      'userID': 1,
-      'date': '2020/01/11',
-      'roomNumber': 4,
-      'roomServiceCharges': []
-    });
+    expect(customer.bookings).to.deep.equal([
+      {
+        'id': '5fwrgu4i7k55hl6t7',
+        'userID': 1,
+        'date': '2020/01/16',
+        'roomNumber': 5,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6t8',
+        'userID': 1,
+        'date': '2020/01/05',
+        'roomNumber': 3,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6tb',
+        'userID': 1,
+        'date': '2020/01/11',
+        'roomNumber': 4,
+        'roomServiceCharges': []
+      }
+    ]);
   });
 
   it('Should sort bookings by descending date', () => {
     customer.sortBookings();
-    expect(customer.bookings).to.deep.equal({
-      'id': '5fwrgu4i7k55hl6t8',
-      'userID': 1,
-      'date': '2020/01/05',
-      'roomNumber': 3,
-      'roomServiceCharges': []
-    },
-    {
-      'id': '5fwrgu4i7k55hl6tb',
-      'userID': 1,
-      'date': '2020/01/11',
-      'roomNumber': 4,
-      'roomServiceCharges': []
-    },
-    {
-      'id': '5fwrgu4i7k55hl6t7',
-      'userID': 1,
-      'date': '2020/01/16',
-      'roomNumber': 5,
-      'roomServiceCharges': []
-    });
+    expect(customer.bookings).to.deep.equal([
+      {
+        'id': '5fwrgu4i7k55hl6t8',
+        'userID': 1,
+        'date': '2020/01/05',
+        'roomNumber': 3,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6tb',
+        'userID': 1,
+        'date': '2020/01/11',
+        'roomNumber': 4,
+        'roomServiceCharges': []
+      },
+      {
+        'id': '5fwrgu4i7k55hl6t7',
+        'userID': 1,
+        'date': '2020/01/16',
+        'roomNumber': 5,
+        'roomServiceCharges': []
+      }
+    ]);
   });
 
   it('Should return a list of all past bookings', () => {
-    expect(customer.getPastBookings('2020-01-12').to.deep.equal([
+    expect(customer.getPastBookings('2020-01-12')).to.deep.equal([
       {
         'id': '5fwrgu4i7k55hl6t8',
         'userID': 1,
