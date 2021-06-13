3177f993be6a93b3a5f96ad5b8cbd0a3dc7c6b0d
Lauren Kessell <lkessell1@gmail.com>
2021-06-13 14:53:42 -0400

    Import images

--- src/scripts.js
+++ src/scripts.js
@@ -6,5 +6,9 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/junior.jpg'
+import './images/residential.jpg'
+import './images/single.jpg'
+import './images/suite.jpg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
