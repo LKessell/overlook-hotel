12cb55aaa7a919d96330906f36b56cc0e7a65162
Lauren Kessell <lkessell1@gmail.com>
2021-06-14 11:48:05 -0400

    Add function to render room card

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -30,9 +30,9 @@ const domUpdates = {
         </div>
         </article>
         `
-      })
+      });
     } else {
-      container.innerHTML = 'No bookings to display';
+      container.innerHTML = '<p>No bookings to display</p>';
     }
   },
 
@@ -46,6 +46,39 @@ const domUpdates = {
   changeHeading(text, element) {
     element.innerText = text;
   },
+
+  renderRoomCards(container, data) {
+    container.innerHTML = '';
+    if (data.length) {
+      data.forEach(element => {
+        let image = element.roomType.split(' ')[0];
+        container.innerHTML += `
+        <article>
+          <div class="image-wrapper">
+            <img src="./images/${image}.jpg" alt="${element.roomType}">
+            <div class="image-overlay">
+              <button class="more-info"><i class="fas fa-info-circle"></i></button>
+              <div class="overlay-text">
+                <p>Number</p>
+                <p class="counter">${element.number}</p>
+              </div>
+            </div>
+          </div>
+          <section class="article-text">
+            <p class="name-text">${element.roomType}</p>
+            <p class="info-text">Bidet: ${element.bidet}</p>
+            <p class="info-text">Bed Size: ${element.bedSize}</p>
+            <p class="info-text">Number of Beds: ${element.numBeds}</p>
+            <p class="info-text">Cost Per Night:</p>
+            <p class="counter">$${element.costPerNight.toFixed(2)}</p>
+          </section>
+        </article>
+        `;
+      });
+    } else {
+      container.innerHTML = '<p>We are very sorry, but no rooms match your current selections.</p><p>Please try a different date or room type.</p>';
+    }
+  }
 }
 
 export default domUpdates;
